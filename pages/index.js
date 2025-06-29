6460afad6e8f5f6b01c20f01d84f21c2fb410cd8
Uday Rathore <udayvardhan.998@gmail.com>
2025-06-29 09:54:57 +0530

    initial6

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-2xl font-bold">
+      Welcome to DevPath 🚀
+    </div>
+  );
+}
